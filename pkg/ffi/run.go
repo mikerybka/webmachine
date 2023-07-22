3822c6ef0ee3e918d00b04352a52bf61c49771ad
Mike Rybka <merybka@gmail.com>
2023-07-21 20:23:59 -0400

    Use exit status to determin HTTP status

--- pkg/ffi/run.go
+++ pkg/ffi/run.go
@@ -1,6 +1,8 @@
 package ffi
 
 import (
+	"bytes"
+	"fmt"
 	"io"
 	"net/http"
 	"os/exec"
@@ -9,63 +11,35 @@ import (
 func Run(input io.Reader, name string, args ...string) (status int, body []byte, err error) {
 	cmd := exec.Command(name, args...)
 	cmd.Stdin = input
-	stdout, err := cmd.StdoutPipe()
+	stdout := bytes.NewBuffer(nil)
+	stderr := bytes.NewBuffer(nil)
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+	err = cmd.Run()
+	stdoutbytes, _ := io.ReadAll(stdout)
+	stderrbytes, _ := io.ReadAll(stderr)
 	if err != nil {
-		return http.StatusInternalServerError, nil, err
-	}
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		return http.StatusInternalServerError, nil, err
-	}
-	err = cmd.Start()
-	if err != nil {
-		return http.StatusInternalServerError, nil, err
-	}
-	out, err := io.ReadAll(stdout)
-	if err != nil {
-		return http.StatusInternalServerError, out, err
-	}
-	errout, err := io.ReadAll(stderr)
-	if err != nil {
-		return http.StatusInternalServerError, out, err
-	}
-	err = cmd.Wait()
-	if err != nil {
-		return http.StatusInternalServerError, out, err
-	}
-	if len(errout) > 0 {
-		switch string(errout) {
-		case "200":
-			return http.StatusOK, out, nil
-		case "201":
-			return http.StatusCreated, out, nil
-		case "204":
-			return http.StatusNoContent, out, nil
-		case "400":
-			return http.StatusBadRequest, out, nil
-		case "401":
-			return http.StatusUnauthorized, out, nil
-		case "403":
-			return http.StatusForbidden, out, nil
-		case "404":
-			return http.StatusNotFound, out, nil
-		case "405":
-			return http.StatusMethodNotAllowed, out, nil
-		case "409":
-			return http.StatusConflict, out, nil
-		case "500":
-			return http.StatusInternalServerError, out, nil
-		case "501":
-			return http.StatusNotImplemented, out, nil
-		case "503":
-			return http.StatusServiceUnavailable, out, nil
-		case "504":
-			return http.StatusGatewayTimeout, out, nil
-		case "507":
-			return http.StatusInsufficientStorage, out, nil
-		default:
-			return http.StatusInternalServerError, out, err
+		status = http.StatusInternalServerError
+		if len(stdoutbytes) > 0 {
+			err = fmt.Errorf("%s\nstdout:\n%s", err, stdoutbytes)
 		}
+		if len(stderrbytes) > 0 {
+			err = fmt.Errorf("%s\nstderr:\n%s", err, stderrbytes)
+		}
+		return status, nil, err
 	}
-	return http.StatusOK, out, nil
+
+	exitCode := cmd.ProcessState.ExitCode()
+	fmt.Println(exitCode)
+	if exitCode == 0 {
+		status = 200
+	} else if exitCode >= 100 && exitCode < 600 {
+		status = exitCode
+	} else {
+		status = http.StatusInternalServerError
+	}
+
+	body = stdoutbytes
+
+	return status, body, nil
 }
