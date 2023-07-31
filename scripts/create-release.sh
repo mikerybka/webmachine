bd1b5be88a6a617def91fbc43821d5bab54dc299
Mike Rybka <merybka@gmail.com>
2023-07-31 11:55:37 -0400

    Fix release script

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -3,9 +3,9 @@
 set -e
 
 VERSION=$1
-BUILD_DIR=~/src/github.com/mikerybka/apps/webmachine.dev/builds/webmachine_$V-1_amd64
+BUILD_DIR=~/src/github.com/mikerybka/apps/webmachine.dev/builds/webmachine_$VERSION-1_amd64
 
-if [ -z "$V" ]; then
+if [ -z "$VERSION" ]; then
   echo "Usage: $0 <version>"
   exit 1
 fi
@@ -13,7 +13,7 @@ fi
 mkdir -p $BUILD_DIR/DEBIAN
 cat > $BUILD_DIR/DEBIAN/control <<EOF
 Package: webmachine
-Version: ${V}
+Version: ${VERSION}
 Architecture: amd64
 Maintainer: Mike Rybka <merybka@gmail.com>
 Description: Host static or dynamic websites with ease.
@@ -23,8 +23,5 @@ EOF
 
 mkdir -p $BUILD_DIR/usr/local/bin
 go build -o $BUILD_DIR/usr/local/bin/webmachine github.com/mikerybka/webmachine
-
-mkdir -p $BUILD_DIR/etc/systemd/system
-cp systemd/webmachine.service $BUILD_DIR/etc/systemd/system/webmachine.service
 
 dpkg-deb --build --root-owner-group $BUILD_DIR
